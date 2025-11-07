ed58e569d7a081f2bf2489485973bd74f03256b4
NirvanaBradford <nirvanabradford06@gmail.com>
2025-11-06 20:45:51 -0500

    feat: create database table schema

---
+++ db/queries/user.js
@@ -0,0 +1,16 @@
+import bcrypt from "bcrypt";
+import db from "../client";
+
+export async function createUser(username, password) {
+  const sql = `
+    INSERT INTO user (username, password)
+    VALUES ($1, $2)
+    RETURNING *;
+    `;
+  //=== i followed guided practice for this part===
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const {
+    rows: [user],
+  } = await db.query(sql, [username, hashedPassword]);
+  return user;
+}
